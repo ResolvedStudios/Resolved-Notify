e5878103bed9fd40c22573e4afcfb710c4668bbc
ResolvedStudios <ResolvedStudios@outlook.com>
2025-02-13 16:00:29 -0500

    (Updated Wrong File)

--- Resolved-Notify/client.lua
+++ Resolved-Notify/client.lua
@@ -1,40 +1,17 @@
--- Client-Only Notification Event
 RegisterNetEvent('customNotify:Send')
 AddEventHandler('customNotify:Send', function(type, message, duration)
+    local sound = "notify" 
+
     SendNUIMessage({
         action = 'show',
-        type = type,
+        type = type, 
         message = message,
-        duration = duration or 3000,
-        sound = "notify"
+        duration = duration or 3000, 
+        sound = sound
     })
 end)
 
--- Global Notification Event (All Players)
-RegisterNetEvent('customNotify:SendToAll')
-AddEventHandler('customNotify:SendToAll', function(type, message, duration)
-    SendNUIMessage({
-        action = 'show',
-        type = type,
-        message = message,
-        duration = duration or 3000,
-        sound = "notify"
-    })
-end)
-
--- Specific Player Notification Event (Targeted by Server)
-RegisterNetEvent('customNotify:SendToPlayer')
-AddEventHandler('customNotify:SendToPlayer', function(type, message, duration)
-    SendNUIMessage({
-        action = 'show',
-        type = type,
-        message = message,
-        duration = duration or 3000,
-        sound = "notify"
-    })
-end)
-
--- Test Command for Client Notifications
+-- Test notification command /testnotify 
 RegisterCommand('testnotify', function()
     TriggerEvent('customNotify:Send', 'success', 'This is a success message!', 5000)
     Wait(2000)
